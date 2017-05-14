ea4543a1be85067c65f814955bddfa26ebb249c8
Danilo <dnp1@users.noreply.github.com>
2017-05-14 12:45:31 -0300

    User implementation

--- server/user/user.go
+++ server/user/user.go
@@ -4,6 +4,7 @@ import (
     "github.com/pkg/errors"
     "database/sql"
     "github.com/jmoiron/sqlx"
+    "golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -18,18 +19,39 @@ type User interface {
 
 type Builder struct {
     DB *sql.DB
+    BCryptCost int
 }
 
 func (builder Builder) Build() User {
+    if builder.BCryptCost == 0 {
+        builder.BCryptCost = bcrypt.DefaultCost
+    }
     return &user{
         db: sqlx.NewDb(builder.DB, ""),
+        bCryptCost: builder.BCryptCost ,
     }
 }
 
 type user struct {
     db *sqlx.DB
+    bCryptCost int
 }
 
 func (u *user) Create(username string, password string, passwordConfirmation string) error {
+    if password != passwordConfirmation {
+        return ErrPasswordConfirmationDoesNotMatch
+    }
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), u.bCryptCost)
+    if err != nil {
+        return err
+    }
+    const query = `INSERT INTO "user"("username", "password") VALUES('1121212', 'sadd')
+        ON CONFLICT ON CONSTRAINT "uq_username" DO NOTHING RETURNING id;`
+    var id int64;
+    if err := u.db.QueryRow(query, username, string(hashedPassword)).Scan(&id); err == sql.ErrNoRows {
+        return ErrUsernameAlreadyTaken
+    } else if err != nil {
+        return err
+    }
     return nil
 }
